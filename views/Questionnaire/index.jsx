43e749f5c7095a8bea234c83435ddb715c9a0664
Sawyer Jones <sawyerjones032@gmail.com>
2024-06-19 10:43:57 -0700

    removed TTS & added last question w/o skip logic

--- views/Questionnaire/index.jsx
+++ views/Questionnaire/index.jsx
@@ -1,6 +1,4 @@
 import {useEffect, useState} from 'react';
-import Voice from '@react-native-voice/voice';
-import TTS from 'react-native-tts';
 import Geolocation from '@react-native-community/geolocation';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {Alert, Platform} from 'react-native';
@@ -24,41 +22,9 @@ export const Questionnaire = () => {
     SAVED: 'SAVED',
   };
 
-  const TTS_STATES = {
-    STARTED: 'STARTED',
-    FINISHED: 'FINISHED',
-    CANCELLED: 'CANCELLED',
-  };
-
-  const numbersInWords = {
-    one: 1,
-    to: 2,
-    too: 2,
-    two: 2,
-    three: 3,
-    four: 4,
-    for: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9,
-  };
-
-  const VOICE_COMMANDS = {
-    PREVIOUS_QUESTION: 'previous question',
-    CANCEL_QUESTIONNAIRE: 'cancel questionnaire',
-  };
-
   // RECORDING
   // const [isRecording, setIsRecording] = useState(false);
   const [isManualNavigation, setIsManualNavigation] = useState(false);
-
-  // VOICE
-  const [partialResults, setPartialResults] = useState('');
-
-  // TTS
-  const [ttsState, setTtsState] = useState();
 
   // QUESTIONS
   const [questions, setQuestions] = useState([]);
@@ -73,122 +39,9 @@ export const Questionnaire = () => {
     header: "",
   });
 
-  const stopRecording = () => {
-    return Voice.stop();
-  };
-
-  const startRecording = () => {
-    // setIsRecording(true);
-    Voice.start('en-US');
-    setTimeout(() => {
-      stopRecording();
-      // setIsRecording(false);
-    }, 5000); // Stop the recording after 5 seconds
-  };
-
-  // VOICE HANDLERS
-  function onSpeechStart(e) {
-    console.log('onSpeechStart: ', e);
-  }
-  function onSpeechEnd(e) {
-    console.log('onSpeechEnd: ', e);
-  }
-  function onSpeechPartialResults(e) {
-    console.log('onSpeechPartialResults: ', e);
-    const milis = new Date().getTime();
-    // if (!isRecording) return; // Ignore results if not recording
-    // console.log('onSpeechPartialResults: ', e);
-  const result = e.value[0].toLowerCase();
-  if (result.includes(VOICE_COMMANDS.PREVIOUS_QUESTION)) {
-    goToPreviousQuestion();
-  } else if (result.includes(VOICE_COMMANDS.CANCEL_QUESTIONNAIRE)) {
-    cancelQuestionnaire();
-
-  }
-    setPartialResults(prevState => {
-      // TODO:
-      // HACK 3: LOCKING INPUT FROM VOICE IF WE HAVE DUPLICATE RESULTS FIRING. THIS IS CAUSED BY VOICE ENGINE GENERATING CONTEXT AND POTENTIALLY ALTERING THE SENTENCE
-      // SOMETIMES ALTERATIONS IN THE RESULTS DO NOT HAPPEN AND THE SAME RESULT IS FIRED TWICE
-      if (
-        prevState &&
-        e.value[0] === prevState.results[0] &&
-        milis - prevState.collectedAt <= TIME_FOR_LOCK
-      ) {
-        // console.log(
-        //   'returning',
-        //   prevState,
-        //   e.value[0] === prevState.results[0],
-        //   milis - prevState.collectedAt <= 1000,
-        // );
-        return prevState;
-      }
-
-      // TODO:
-      // HACK 2: check for case when For and Four are fired sequenctially (we transform the text of previous answer and current answer to numerical values and compare the two)
-      if (prevState && milis - prevState.collectedAt <= TIME_FOR_LOCK) {
-        const newText = e.value[0];
-        const oldText = prevState.results[0];
-        if (!oldText || !newText) return prevState;
-        const newWords = newText.split(' ');
-        const newNumber = newWords[newWords.length - 1].toLowerCase();
-        const oldWords = oldText.split(' ');
-        const oldNumber = oldWords[oldWords.length - 1].toLowerCase();
-        if (
-          numbersInWords[newNumber] &&
-          numbersInWords[oldNumber] &&
-          numbersInWords[newNumber] === numbersInWords[oldNumber]
-        ) {
-          return prevState;
-        }
-      }
-      console.log('Setting new value: ', e.value);
-      return {results: e.value, collectedAt: milis};
-    });
-  }
-
-  function onSpeechError(e) {
-    console.log('onSpeechError', e);
-  }
-
-  // TTS HANDLERS
-  function ttsStartHandler(e) {
-    console.log('TTS STARTED');
-    setTtsState(TTS_STATES.STARTED);
-  }
-
-  function ttsFinishHandler(e) {
-    console.log('TTS FINISHED: ', e);
-    setTtsState(TTS_STATES.FINISHED);
-  }
-
-  function ttsCancelHandler(e) {
-    console.log('TTS CANCELLED: ', e);
-    setTtsState(TTS_STATES.CANCELLED);
-  }
-
   useEffect(() => {
     // INIT FUNCTION
     async function init() {
-      try {
-        const ttsInitStatus = await TTS.getInitStatus();
-        if (!ttsInitStatus) {
-          throw new Error('TTS initialization Failed');
-        }
-        TTS.addEventListener('tts-start', ttsStartHandler);
-        TTS.addEventListener('tts-finish', ttsFinishHandler);
-        TTS.addEventListener('tts-cancel', ttsCancelHandler);
-      } catch (error) {
-        // TODO: HANDLE ERRORS IN TTS INITIALIZATION
-        console.log('TTS INITIALIZATION ERROR', error);
-      }
-      Voice.onSpeechStart = onSpeechStart;
-      Voice.onSpeechEnd = onSpeechEnd;
-
-      if (Platform.OS === 'android')
-        Voice.onSpeechResults = onSpeechPartialResults;
-      else Voice.onSpeechPartialResults = onSpeechPartialResults;
-      Voice.onSpeechError = onSpeechError;
-
       try {
         const questions = await questionService.fetchQuestions();
         setQuestions(questions);
@@ -199,17 +52,7 @@ export const Questionnaire = () => {
     }
     init();
 
-    return () => {
-      if (Platform.OS === 'ios') {
-        TTS.removeEventListener('tts-start', ttsStartHandler);
-        TTS.removeEventListener('tts-finish', ttsFinishHandler);
-        TTS.removeEventListener('tts-cancel', ttsCancelHandler);
-      }
-      TTS.stop().catch(error => console.log('TTS STOP FAILED', error));
-      Voice.destroy().catch(error =>
-        console.log('DESTORYING VOICE FAILED', error),
-      );
-    };
+    return () => {};
   }, []);
 
   const getWeather = async (lat, lon) => {
@@ -262,11 +105,6 @@ export const Questionnaire = () => {
   const goToPreviousQuestion = async () => {
     // Stop any ongoing recording or TTS
     setIsManualNavigation(true);
-    try {
-      await TTS.stop();
-    } catch (error) {
-      console.log('Error stopping TTS in goToPreviousQuestion', error);
-    }
 
     setQStatus(q => {
       if (q.questionIdx === 0) return q; // Check to prevent index going below 0
@@ -284,14 +122,6 @@ export const Questionnaire = () => {
 
   const nextQuestion = async () => {
     setIsManualNavigation(true);
-    try {
-      await TTS.stop();
-    } catch (error) {
-      console.log('TTS stop failed at next question');
-    }
-    // if (isRecording) {
-    //   await new Promise(resolve => setTimeout(resolve, 5000));
-    // }
     if (qStatus.questionIdx + 1 >= questions.length) {
       return setQStatus(q => ({...q, state: QUESTIONNAIRE_STATES.LOADING}));
     }
@@ -299,7 +129,6 @@ export const Questionnaire = () => {
   };
 
   const selectAnswer = answer => {
-    stopRecording().then(
       setQStatus(q => {
         const lastAnswerSet = new Date().getTime();
         return {
@@ -313,15 +142,9 @@ export const Questionnaire = () => {
           ],
           lastAnswerSet,
         };
-      }),
-    );
+      });
     setIsManualNavigation(false);
   };
-
-  function getChoiceFromSpeech(text) {
-    const match = text.match(/choice (\d+)/i);
-    return match ? parseInt(match[1], 10) : null;
-  }
 
   const startQuestionnaire = () => {
     return setQStatus(q => ({...q, state: QUESTIONNAIRE_STATES.STARTED}));
@@ -337,36 +160,11 @@ export const Questionnaire = () => {
   };
 
   const cancelQuestionnaire = async () => {
-    try {
-      await TTS.stop();
-      await stopRecording();
-    } catch (error) {
-      console.error("Error cancelling TTS or Voice recording:", error);
-    }
-  
     setQStatus({
       state: QUESTIONNAIRE_STATES.BEFORE_STARTING,
       questionIdx: 0,
       answeredQuestions: [],
       externalData: {},
-    });
-  };
-
-  const readQuestion = async () => {
-    const {question, answers} = questions[qStatus.questionIdx];
-
-    const text =
-      'Question ' + (qStatus.questionIdx + 1) + ', ' + question + '; ';
-
-    const ans = answers
-      .map((ans, index) => {
-        if (qStatus.questionIdx == 0) return index + 1 + ', ' + ans;
-        return 'choice ' + (index + 1) + ', ';
-      })
-      .join();
-
-    TTS.getInitStatus().then(() => {
-      TTS.speak(text + ans);
     });
   };
 
@@ -410,7 +208,7 @@ export const Questionnaire = () => {
     }
   };
 
-  // CHECK CHAGES ON SAVED ANSWERED QUESTIONS
+  // CHECK CHANGES ON SAVED ANSWERED QUESTIONS
   useEffect(() => {
     if (qStatus.state === QUESTIONNAIRE_STATES.BEFORE_STARTING || isManualNavigation) {
     setIsManualNavigation(false);
@@ -419,56 +217,8 @@ export const Questionnaire = () => {
     nextQuestion();
   }, [qStatus.answeredQuestions]);
 
-  // READ QUESTION ON INDEX UPDATE
-  useEffect(() => {
-    if (qStatus.state !== QUESTIONNAIRE_STATES.STARTED) return;
-    readQuestion();
-  }, [qStatus.questionIdx]);
-
-  // HANDLE PARTIAL RESULT CHANGES
-  useEffect(() => {
-    console.log('FIRED');
-    if (qStatus.state === QUESTIONNAIRE_STATES.BEFORE_STARTING) return;
-
-    const {answers} = questions[qStatus.questionIdx];
-
-    const text = partialResults.results[0];
-    if (!text) return;
-
-    const choiceNumber = getChoiceFromSpeech(text);
-    if (choiceNumber && choiceNumber <= answers.length) {
-      console.log('FOUND FOUND FOUND via choice');
-      selectAnswer(answers[choiceNumber - 1]);
-      return;
-    }
-
-    const words = text.split(' ');
-    const number = words[words.length - 1].toLowerCase();
-    if (numbersInWords[number] && numbersInWords[number] <= answers.length) {
-      // found.set(true);
-      console.log('FOUND FOUND FOUND');
-      selectAnswer(answers[numbersInWords[number] - 1]);
-    } else {
-      for (const text of partialResults.results) {
-        for (const answ of answers) {
-          console.log(answ);
-          if (text.toLowerCase().includes(answ.toLowerCase())) {
-            // found.set(true);
-            console.log('FOUND FOUND FOUND');
-            selectAnswer(answ);
-            return;
-          }
-        }
-      }
-    }
-  }, [partialResults.results]);
-
   // QUESTIONNAIRE STATE CHANGE
   useEffect(() => {
-    if (qStatus.state == QUESTIONNAIRE_STATES.STARTED) {
-      if (qStatus.questionIdx === 0) readQuestion();
-    }
-
     if (qStatus.state == QUESTIONNAIRE_STATES.LOADING) {
       getExternalInformation().then(information => {
         const [location, weather] = information;
@@ -486,26 +236,6 @@ export const Questionnaire = () => {
       });
     }
   }, [qStatus.state]);
-
-  useEffect(() => {
-    if (ttsState === TTS_STATES.FINISHED) {
-      const time = new Date().getTime();
-
-      // TODO:
-      // HACK 1: THIS IS NEEDED TO AVOID STARTING THE RECORDING IF WE MANUALLY SELECT THE ANSWER
-      // TTS.FINISHED IS FIRED ON TTS.STOP as well as when TTS.SPEAK finishes talking
-      if (time - qStatus.lastAnswerSet <= TIME_FOR_LOCK) return;
-      console.log('STARTING RECORDING');
-      startRecording();
-    }
-    if (ttsState === TTS_STATES.CANCELLED) {
-      try {
-        stopRecording();
-      } catch (error) {
-        console.log('STOP RECORDING FAILED AT TTS STATE');
-      }
-    }
-  }, [ttsState]);
 
   // UI LOGIC
 
@@ -638,7 +368,7 @@ export const Questionnaire = () => {
           />
         </View>
       )}
-        <View style={{marginTop: 20}}>
+        <View style={{marginTop: 20, marginBottom: 40}}>
           <Button
             title="Cancel Questionnaire"
             buttonStyle={{
